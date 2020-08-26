7f10e13c65835d2ad1aef82b2bd58c98707fd89b
JasEmaz <68593957+JasEmaz@users.noreply.github.com>
2020-08-26 15:27:57 +0000

    back-end added

---
+++ back-end/venv/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/covid')
+def tracker():
+    data = dict(
+        total=1300,
+        active=300,
+        recovered=1000,
+        deaths=50,
+        ncases=150
+    )
+    return jsonify(data)
+
